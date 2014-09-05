201ad717ff822b0e1acc01b60deeb31b7bed812a
Ralf <ralf.globisch@gmail.com>
2014-09-05 14:14:36 +0200

    Implemented basic dummy adaptation

--- src/libs/SimpleRateAdaptation/SimpleRateAdaptation.cpp
+++ src/libs/SimpleRateAdaptation/SimpleRateAdaptation.cpp
@@ -5,15 +5,45 @@ namespace lme
 {
 
 SimpleRateAdaptation::SimpleRateAdaptation()
+  :m_dPreviousAverage(-1.0),
+  m_lastPacketNumReceived(0),
+  nCounter(0),
+  nCurrentChannel(0),
+  bUp(true)
 {
 
 }
 
-SwitchDirection SimpleRateAdaptation::getRateAdaptAdvice(const RtpTransmissionStats& stats)
+lme::SwitchDirection SimpleRateAdaptation::getRateAdaptAdvice(const lme::RtpTransmissionStats& stats)
 {
-  VLOG(2) << "Rate adaptation received: LastPacketNumReceived: " << stats;
-  // NOOP
-  return STAY;
+  // Get packet num
+  unsigned packetNum = stats.LastPacketNumReceived;
+  if (packetNum == m_lastPacketNumReceived)
+  {
+    // There's no new information
+    VLOG(5) << "Staying: no new info";
+    return lme::STAY;
+  }
+
+  lme::SwitchDirection eSwitchDirection = lme::STAY;
+
+  if (bUp)
+  {
+    ++nCurrentChannel;
+    VLOG(5) << "Switching up";
+    eSwitchDirection = lme::SWITCH_UP;
+    if (nCurrentChannel == 7)
+      bUp = false;
+  }
+  else
+  {
+    VLOG(5) << "Switching down";
+    --nCurrentChannel;
+    eSwitchDirection = lme::SWITCH_DOWN;
+    if (nCurrentChannel == 0)
+      bUp = true;
+  }
+  return eSwitchDirection;
 }
 
 } // lme
--- src/libs/SimpleRateAdaptation/SimpleRateAdaptation.h
+++ src/libs/SimpleRateAdaptation/SimpleRateAdaptation.h
@@ -4,15 +4,36 @@
 namespace lme
 {
 
-class SimpleRateAdaptation : public IRateAdaptation
+/**
+ * @brief SimpleRoundTripDelayAnalyser
+ *  This simple round trip delay analyser simply starts on channel 3 (with indexing starting at 0)
+ *  It then switches down every 150 frames until it reaches the minimum band width channel (0).
+ *  It switches up every 150 frames until it reaches th emaximum bandwidth channel and then repeats the
+ *  process. This implementation assumes that the channel has 8 different bit rates!!!!
+ */
+class SimpleRateAdaptation : public lme::IRateAdaptation 
 {
 public:
+
+  /**
+   * @brief Constructor
+   */
   SimpleRateAdaptation();
 
-  virtual SwitchDirection getRateAdaptAdvice(const RtpTransmissionStats& stats);
+  /**
+   * @brief TODO: describe implementation
+   */
+  lme::SwitchDirection getRateAdaptAdvice(const lme::RtpTransmissionStats& stats);
 
 private:
 
+  bool isTimeToMeasure();
+
+	double m_dPreviousAverage;
+	unsigned m_lastPacketNumReceived;
+	int nCounter;
+	int nCurrentChannel;
+	bool bUp;
 };
 
 } // lme
