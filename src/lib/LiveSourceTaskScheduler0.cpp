dad418dc5e5c4d9fbc542e44c6a71a392978cc26
miseri <ralf.globisch@gmail.com>
2014-11-06 16:06:16 +0200

    Improved processing of samples within media sessions

--- src/lib/LiveSourceTaskScheduler0.cpp
+++ src/lib/LiveSourceTaskScheduler0.cpp
@@ -23,167 +23,193 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 namespace lme
 {
 
-const unsigned MAX_SAMPLES_TO_BE_PROCESSED_IN_EVENT_LOOP = 10;
-const unsigned MAX_GROUP_SAMPLES_TO_BE_PROCESSED_IN_EVENT_LOOP = 5;
+  const unsigned MAX_REVOLUTIONS = 15;
+  const unsigned MAX_SAMPLES_TO_BE_PROCESSED_IN_EVENT_LOOP = 30;
 
-LiveSourceTaskScheduler0::LiveSourceTaskScheduler0(ChannelManager& channelManager)
-  : m_uMaxDelayTime(100000), // default time of once per second,
-  BasicTaskScheduler(m_uMaxDelayTime),
-  m_channelManager(channelManager)
-{
-
-}
-
-LiveSourceTaskScheduler0::~LiveSourceTaskScheduler0()
-{
-
-}
-
-void LiveSourceTaskScheduler0::doEventLoop( char* watchVariable )
-{
-  // Repeatedly loop, handling readable sockets and timed events:
-  while (1) 
+  LiveSourceTaskScheduler0::LiveSourceTaskScheduler0(ChannelManager& channelManager)
+    : m_uMaxDelayTime(100), // default time of once per second,
+    BasicTaskScheduler(m_uMaxDelayTime),
+    m_channelManager(channelManager)
   {
-    if (watchVariable != NULL)
+
+  }
+
+  LiveSourceTaskScheduler0::~LiveSourceTaskScheduler0()
+  {
+
+  }
+
+  void LiveSourceTaskScheduler0::doEventLoop(char* watchVariable)
+  {
+    // Repeatedly loop, handling readable sockets and timed events:
+    while (1)
     {
-      if (*watchVariable != 0)
+      if (watchVariable != NULL)
       {
-        bool bExitEventLoop = false;
-        switch ((*watchVariable))
+        if (*watchVariable != 0)
         {
-          // Exit event loop
-        case 1:
+          bool bExitEventLoop = false;
+          switch ((*watchVariable))
+          {
+            // Exit event loop
+          case 1:
           {
             bExitEventLoop = true;
             break;
           }
+          }
+          if (bExitEventLoop)
+          {
+            // Break out the while
+            break;
+          }
         }
-        if (bExitEventLoop)
+      }
+
+#ifdef DEBUG
+      VLOG(2) << "Calling processLiveSources";
+#endif
+      processLiveSources();
+#ifdef DEBUG
+      VLOG(2) << "Done processLiveSources, calling SingleStep";
+#endif
+      // Call the live media single step
+      SingleStep(m_uMaxDelayTime);
+#ifdef DEBUG
+      VLOG(2) << "Done  SingleStep";
+#endif
+    }
+  }
+
+  void LiveSourceTaskScheduler0::addMediaSubsession(uint32_t  uiChannelId, uint32_t uiSourceId, LiveMediaSubsession* pMediaSubsession)
+  {
+#ifdef DEBUG
+    VLOG(2) << "LiveSourceTaskScheduler0::addMediaSubsession: " << uiChannelId << " source: " << uiSourceId;
+#endif
+    MediaSessionMap_t::iterator it = m_mMediaSessions.find(uiChannelId);
+    if (it != m_mMediaSessions.end())
+    {
+      MediaSession_t::iterator it2 = it->second.find(uiSourceId);
+      if (it2 != it->second.end())
+      {
+        LOG(WARNING) << "Media subsession with source ID " << uiSourceId << " already registered in scheduler!";
+        assert(false);
+      }
+      else
+      {
+        VLOG(5) << "Registering media subsession for channel: " << uiChannelId
+          << " source: " << uiSourceId;
+        it->second[uiSourceId] = pMediaSubsession;
+      }
+    }
+    else
+    {
+      VLOG(5) << "Registering media subsession for channel: " << uiChannelId
+        << " source: " << uiSourceId;
+      m_mMediaSessions[uiChannelId][uiSourceId] = pMediaSubsession;
+    }
+  }
+
+  void LiveSourceTaskScheduler0::removeMediaSubsession(uint32_t  uiChannelId, uint32_t uiSourceId, LiveMediaSubsession* pMediaSubsession)
+  {
+#ifdef DEBUG
+    VLOG(5) << "Trying to remove media subsession with channel: " << uiChannelId << " source: " << uiSourceId;
+#endif
+
+    MediaSessionMap_t::iterator it = m_mMediaSessions.find(uiChannelId);
+    if (it != m_mMediaSessions.end())
+    {
+      MediaSession_t::iterator it2 = it->second.find(uiSourceId);
+      if (it2 != it->second.end())
+      {
+        VLOG(5) << "Removing media subsession for channel: " << uiChannelId << " source: " << uiSourceId;
+        m_mMediaSessions.erase(it);
+      }
+      else
+      {
+        LOG(WARNING) << "Unable to remove media subsession with channel: " << uiChannelId << " source: " << uiSourceId;
+      }
+    }
+    else
+    {
+      LOG(WARNING) << "Unable to remove media subsession with ID: " << uiChannelId;
+    }
+  }
+
+  void LiveSourceTaskScheduler0::processLiveSources()
+  {
+#ifdef DEBUG
+    VLOG(2) << "Processing live sources";
+#endif
+    uint32_t uiSessionCount = 0;
+    uint32_t uiSampleCount = 0;
+    bool bExit = false;
+    // try and retrieve a sample for each channel
+    // warning: the if condition of the for needs to be re-thought if there are more sessions
+    // i.e. some sessions may never be reached if the for always starts at begin and exits when 
+    // MAX_SAMPLES_TO_BE_PROCESSED_IN_EVENT_LOOP is reached. For the purpose fo the single
+    // live media session this should suffice
+    while (uiSessionCount < MAX_REVOLUTIONS)
+    {
+      for (MediaSessionMap_t::iterator it = m_mMediaSessions.begin();
+        it != m_mMediaSessions.end() && uiSampleCount < MAX_SAMPLES_TO_BE_PROCESSED_IN_EVENT_LOOP;
+        ++it)
+      {
+#ifdef DEBUG
+        VLOG(2) << "Retrieving samples for channel: " << it->first;
+#endif
+        for (MediaSession_t::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2)
         {
-          // Break out the while
+#ifdef DEBUG
+          VLOG(2) << "Retrieving samples for channel " << it->first << " source " << it2->first;
+#endif
+          boost::optional<MediaSample> pMediaSample = m_channelManager.getMedia(it->first, it2->first);
+          if (pMediaSample)
+          {
+            ++uiSampleCount;
+#ifdef DEBUG
+            VLOG(2) << "Got media sample for channel " << it->first << " source " << it2->first << " PTS: " << pMediaSample->getPresentationTime();
+#endif
+            // make sure channel and source ids are set
+            pMediaSample->setChannelId(it->first);
+            pMediaSample->setSourceId(it2->first);
+#ifdef DEBUG
+            VLOG(2) << "Calling addMediaSample for channel " << it->first << " source " << it2->first << " PTS: " << pMediaSample->getPresentationTime();
+#endif
+            it2->second->addMediaSample(*pMediaSample);
+#ifdef DEBUG
+            VLOG(2) << "Done calling addMediaSample for channel " << it->first << " source " << it2->first << " PTS: " << pMediaSample->getPresentationTime();
+#endif
+            if (uiSampleCount >= MAX_SAMPLES_TO_BE_PROCESSED_IN_EVENT_LOOP)
+            {
+              // set exit condition of while
+              uiSessionCount = MAX_REVOLUTIONS;
+              break;
+            }
+          }
+        }
+        if (uiSampleCount >= MAX_SAMPLES_TO_BE_PROCESSED_IN_EVENT_LOOP)
+        {
           break;
         }
       }
+      ++uiSessionCount;
     }
-
-#ifdef DEBUG
-    VLOG(2) << "Calling processLiveSources";
-#endif
-    processLiveSources();
-#ifdef DEBUG
-    VLOG(2) << "Done processLiveSources, calling SingleStep";
-#endif
-    // Call the live media single step
-    SingleStep(m_uMaxDelayTime);
-#ifdef DEBUG
-    VLOG(2) << "Done  SingleStep";
+#if 0
+    VLOG(5) << "Processed " << uiSampleCount << " samples in event loop";
 #endif
   }
-}
 
-void LiveSourceTaskScheduler0::addMediaSubsession(uint32_t  uiChannelId, uint32_t uiSourceId, LiveMediaSubsession* pMediaSubsession)
-{
-#ifdef DEBUG
-  VLOG(2) << "LiveSourceTaskScheduler0::addMediaSubsession: " << uiChannelId << " source: " << uiSourceId;
-#endif
-  MediaSessionMap_t::iterator it = m_mMediaSessions.find(uiChannelId);
-  if (it != m_mMediaSessions.end())
+  void LiveSourceTaskScheduler0::processLiveMediaSessions()
   {
-    MediaSession_t::iterator it2 = it->second.find(uiSourceId);
-    if (it2 != it->second.end())
+    VLOG(15) << "LiveSourceTaskScheduler0::processLiveMediaSessions()";
+    for (MediaSessionMap_t::iterator it = m_mMediaSessions.begin(); it != m_mMediaSessions.end(); ++it)
     {
-      LOG(WARNING) << "Media subsession with source ID " << uiSourceId << " already registered in scheduler!";
-      assert(false);
-    }
-    else
-    {
-      VLOG(5) << "Registering media subsession for channel: " << uiChannelId 
-              << " source: " << uiSourceId;
-      it->second[uiSourceId] = pMediaSubsession;
-    }
-  }
-  else
-  {
-    VLOG(5) << "Registering media subsession for channel: " << uiChannelId
-      << " source: " << uiSourceId;
-    m_mMediaSessions[uiChannelId][uiSourceId] = pMediaSubsession;
-  }
-}
-
-void LiveSourceTaskScheduler0::removeMediaSubsession(uint32_t  uiChannelId, uint32_t uiSourceId, LiveMediaSubsession* pMediaSubsession)
-{
-#ifdef DEBUG
-  VLOG(5) << "Trying to remove media subsession with channel: " << uiChannelId << " source: " << uiSourceId;
-#endif
-
-  MediaSessionMap_t::iterator it = m_mMediaSessions.find(uiChannelId);
-  if (it != m_mMediaSessions.end())
-  {
-    MediaSession_t::iterator it2 = it->second.find(uiSourceId);
-    if (it2 != it->second.end())
-    {
-      VLOG(5) << "Removing media subsession for channel: " << uiChannelId << " source: " << uiSourceId;
-      m_mMediaSessions.erase(it);
-    }
-    else
-    {
-      LOG(WARNING) << "Unable to remove media subsession with channel: " << uiChannelId << " source: " << uiSourceId;
-    }
-  }
-  else
-  {
-    LOG(WARNING) << "Unable to remove media subsession with ID: " << uiChannelId;
-  }
-}
-
-void LiveSourceTaskScheduler0::processLiveSources()
-{
-#ifdef DEBUG
-  VLOG(2) << "Processing live sources";
-#endif
-  // try and retrieve a sample for each channel
-  for (MediaSessionMap_t::iterator it = m_mMediaSessions.begin(); it != m_mMediaSessions.end(); ++it)
-  {
-#ifdef DEBUG
-    VLOG(2) << "Retrieving samples for channel: " << it->first;
-#endif
-    for (MediaSession_t::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2)
-    {
-#ifdef DEBUG
-      VLOG(2) << "Retrieving samples for channel " << it->first << " source " << it2->first;
-#endif
-      boost::optional<MediaSample> pMediaSample = m_channelManager.getMedia(it->first, it2->first);
-      if (pMediaSample)
+      for (MediaSession_t::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2)
       {
-#ifdef DEBUG
-        VLOG(2) << "Got media sample for channel " << it->first << " source " << it2->first << " PTS: " << pMediaSample->getPresentationTime();
-#endif
-        // make sure channel and source ids are set
-        pMediaSample->setChannelId(it->first);
-        pMediaSample->setSourceId(it2->first);
-#ifdef DEBUG
-        VLOG(2) << "Calling addMediaSample for channel " << it->first << " source " << it2->first << " PTS: " << pMediaSample->getPresentationTime();
-#endif
-        it2->second->addMediaSample(*pMediaSample);
-#ifdef DEBUG
-        VLOG(2) << "Done calling addMediaSample for channel " << it->first << " source " << it2->first << " PTS: " << pMediaSample->getPresentationTime();
-#endif
+        it2->second->processClientStatistics();
       }
     }
   }
-}
-
-void LiveSourceTaskScheduler0::processLiveMediaSessions()
-{
-  VLOG(15) << "LiveSourceTaskScheduler0::processLiveMediaSessions()";
-  for (MediaSessionMap_t::iterator it = m_mMediaSessions.begin(); it != m_mMediaSessions.end(); ++it)
-  {
-    for (MediaSession_t::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2)
-    {
-      it2->second->processClientStatistics();
-    }
-  }
-}
 
 } // lme
-
