158658809795ce42ee0c5191e786c28e44b14976
miseri <ralf.globisch@gmail.com>
2014-11-06 16:08:12 +0200

    Tested different queing mechanism to solve sync and buffering problem

--- include/Media/PacketManagerMediaChannel.h
+++ include/Media/PacketManagerMediaChannel.h
@@ -17,6 +17,7 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 // Copyright (c) 2014 CSIR.  All rights reserved.
 #pragma once
 #define LOCK_FREE
+#define SINGLE_QUEUE
 #ifdef LOCK_FREE
 #include <boost/lockfree/spsc_queue.hpp>
 #else
@@ -29,50 +30,58 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 namespace lme
 {
 
-/**
- * @brief Implementation of packet manager-based media channel
- *
- * This implementation will test using the lock-free queue implementations
- * of the boost libraries.
- */
-class PacketManagerMediaChannel : public MediaChannel
-{
-public:
-  /**
-   * @brief Constructor
-   */
-  PacketManagerMediaChannel(uint32_t uiChannelId);
-  /**
-   * @brief The subclass must implement delivery of video media samples to the media sink
-   */
-  virtual boost::system::error_code deliverVideo(uint32_t m_uiChannelId, const std::vector<MediaSample>& mediaSamples);
-  /**
-   * @brief The subclass must implement delivery of audio media samples to the media sink
-   */
-  virtual boost::system::error_code deliverAudio(uint32_t m_uiChannelId, const std::vector<MediaSample>& mediaSamples);
-  /**
-   * @brief This method returns a video media sample if available, and a null pointer otherwise
-   */
-  boost::optional<MediaSample> getVideo();
-  /**
-   * @brief This method returns a video media sample if available, and a null pointer otherwise
-   */
-  boost::optional<MediaSample> getAudio();
+  typedef std::pair<int, MediaSample> MediaSample_t;
 
-private:
+  /**
+  * @brief Implementation of packet manager-based media channel
+  *
+  * This implementation will test using the lock-free queue implementations
+  * of the boost libraries.
+  */
+  class PacketManagerMediaChannel : public MediaChannel
+  {
+  public:
+    /**
+    * @brief Constructor
+    */
+    PacketManagerMediaChannel(uint32_t uiChannelId);
+    /**
+    * @brief The subclass must implement delivery of video media samples to the media sink
+    */
+    virtual boost::system::error_code deliverVideo(uint32_t uiChannelId, const std::vector<MediaSample>& mediaSamples);
+    /**
+    * @brief The subclass must implement delivery of audio media samples to the media sink
+    */
+    virtual boost::system::error_code deliverAudio(uint32_t uiChannelId, const std::vector<MediaSample>& mediaSamples);
+    /**
+    * @brief This method returns a video media sample if available, and a null pointer otherwise
+    */
+    boost::optional<MediaSample> getVideo();
+    /**
+    * @brief This method returns a video media sample if available, and a null pointer otherwise
+    */
+    boost::optional<MediaSample> getAudio();
+
+  private:
 #ifdef LOCK_FREE
 #define LF_CAPACITY 10240
-  /// lock free queue to store video media samples in
-  boost::lockfree::spsc_queue<MediaSample, boost::lockfree::capacity<LF_CAPACITY> > m_videoSamples;
-  /// lock free queue to store audio media samples in
-  boost::lockfree::spsc_queue<MediaSample, boost::lockfree::capacity<LF_CAPACITY> > m_audioSamples;
+    /// lock free queue to store video media samples in
+    boost::lockfree::spsc_queue<MediaSample, boost::lockfree::capacity<LF_CAPACITY> > m_videoSamples;
+    /// lock free queue to store audio media samples in
+    boost::lockfree::spsc_queue<MediaSample, boost::lockfree::capacity<LF_CAPACITY> > m_audioSamples;
 #else
-  std::deque<MediaSample> m_videoSamples;
-  std::deque<MediaSample> m_audioSamples;
-  boost::mutex m_videoLock;
-  boost::mutex m_audioLock;
+#ifdef SINGLE_QUEUE
+    /// @typedef media type( 0 = audio, 1= video), media sample
+    std::deque<MediaSample_t> m_mediaSamples;
+    boost::mutex m_mediaLock;
+    double m_dQueueDuration;
+#else
+    std::deque<MediaSample> m_videoSamples;
+    std::deque<MediaSample> m_audioSamples;
+    boost::mutex m_videoLock;
+    boost::mutex m_audioLock;
 #endif
-};
+#endif
+  };
 
 } //lme
-
--- src/lib/PacketManagerMediaChannel.cpp
+++ src/lib/PacketManagerMediaChannel.cpp
@@ -21,88 +21,274 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 namespace lme
 {
 
-PacketManagerMediaChannel::PacketManagerMediaChannel(uint32_t uiChannelId)
-  :MediaChannel(uiChannelId)
-{
+#define AUDIO_TYPE 0
+#define VIDEO_TYPE 1
+#define MAX_QUEUE_DURATION_SECONDS 0.2 // 200 ms
 
-}
+#ifndef LOCK_FREE
 
-boost::system::error_code PacketManagerMediaChannel::deliverVideo(uint32_t m_uiChannelId, const std::vector<MediaSample>& mediaSamples)
-{
-#ifdef LOCK_FREE
-  std::vector<MediaSample>::const_iterator it = m_videoSamples.push(mediaSamples.begin(), mediaSamples.end());
-  assert(it == mediaSamples.end());
-  return boost::system::error_code();
-#else
-  boost::mutex::scoped_lock l(m_videoLock);
-  m_videoSamples.insert(m_videoSamples.end(), mediaSamples.begin(), mediaSamples.end());
-  return boost::system::error_code();
-#endif
-}
-
-boost::system::error_code PacketManagerMediaChannel::deliverAudio(uint32_t m_uiChannelId, const std::vector<MediaSample>& mediaSamples)
-{
-  //    fprintf(stderr, "deliverAudio");
-#ifdef LOCK_FREE
-  std::vector<MediaSample>::const_iterator it = m_audioSamples.push(mediaSamples.begin(), mediaSamples.end());
-  assert(it == mediaSamples.end());
-  return boost::system::error_code();
-#else
-  boost::mutex::scoped_lock l(m_audioLock);
-  m_audioSamples.insert(m_audioSamples.end(), mediaSamples.begin(), mediaSamples.end());
-  return boost::system::error_code();
-#endif
-}
-
-boost::optional<MediaSample> PacketManagerMediaChannel::getVideo()
-{
-#ifdef LOCK_FREE
-  MediaSample mediaSample;
-  if (m_videoSamples.pop(mediaSample))
+  void printQueueInfo(const std::deque<MediaSample_t>& mediaSamples)
   {
-    return boost::optional<MediaSample>(mediaSample);
+    std::ostringstream ostr;
+    for (auto& pair : mediaSamples)
+    {
+      if (pair.first == AUDIO_TYPE)
+      {
+        ostr << "(A, " << pair.second.getPresentationTime() << ") ";
+      }
+      else
+      {
+        ostr << "(V, " << pair.second.getPresentationTime() << ") ";
+      }
+    }
+
+    VLOG(2) << "Queue info: " << ostr.str();
   }
-  else
+
+  void printQueueInfo2(const std::deque<MediaSample_t>& mediaSamples)
   {
-    // empty
+    std::ostringstream ostr;
+    for (auto& pair : mediaSamples)
+    {
+      if (pair.first == AUDIO_TYPE)
+      {
+        ostr << "A";
+      }
+      else
+      {
+        ostr << "V";
+      }
+    }
+
+    VLOG(2) << "Queue info: (1st: " << mediaSamples.front().second.getPresentationTime() << " " << ostr.str();
+  }
+
+#endif
+
+  PacketManagerMediaChannel::PacketManagerMediaChannel(uint32_t uiChannelId)
+    :MediaChannel(uiChannelId)
+#ifndef LOCK_FREE
+#ifdef SINGLE_QUEUE
+    , m_dQueueDuration(0.0)
+#endif
+#endif
+  {
+#ifdef LOCK_FREE
+    VLOG(2) << "Using lock free implementation";
+#else
+    VLOG(2) << "Using mutex-based implementation";
+#endif
+
+#ifdef SINGLE_QUEUE
+    VLOG(2) << "Using single queue for audio and video";
+#else
+    VLOG(2) << "Using separate queues for audio and video";
+#endif
+  }
+
+  boost::system::error_code PacketManagerMediaChannel::deliverVideo(uint32_t uiChannelId, const std::vector<MediaSample>& mediaSamples)
+  {
+#ifdef LOCK_FREE
+    std::vector<MediaSample>::const_iterator it = m_videoSamples.push(mediaSamples.begin(), mediaSamples.end());
+    assert(it == mediaSamples.end());
+    return boost::system::error_code();
+#else
+#ifdef SINGLE_QUEUE
+    boost::mutex::scoped_lock l(m_mediaLock);
+    for (const MediaSample& mediaSample : mediaSamples)
+    {
+      if (m_mediaSamples.empty())
+      {
+        m_mediaSamples.push_back(std::make_pair(VIDEO_TYPE, mediaSample));
+      }
+      else
+      {
+        for (std::deque<MediaSample_t>::reverse_iterator it = m_mediaSamples.rbegin();
+          it != m_mediaSamples.rend(); ++it)
+        {
+          if (mediaSample.getPresentationTime() > it->second.getPresentationTime())
+          {
+            m_mediaSamples.insert(it.base(), std::make_pair(VIDEO_TYPE, mediaSample));
+            break;
+          }
+        }
+      }
+    }
+    m_dQueueDuration = m_mediaSamples.back().second.getPresentationTime() - m_mediaSamples.front().second.getPresentationTime();
+    if (m_dQueueDuration >= MAX_QUEUE_DURATION_SECONDS)
+    {
+      VLOG(5) << "Queue duration is longer than " << MAX_QUEUE_DURATION_SECONDS << "s: " << m_dQueueDuration << "s";
+    }
+#if 0
+    printQueueInfo(m_mediaSamples);
+#else
+    printQueueInfo2(m_mediaSamples);
+#endif
+#else
+    boost::mutex::scoped_lock l(m_videoLock);
+    m_videoSamples.insert(m_videoSamples.end(), mediaSamples.begin(), mediaSamples.end());
+#endif
+    return boost::system::error_code();
+#endif
+  }
+
+  boost::system::error_code PacketManagerMediaChannel::deliverAudio(uint32_t uiChannelId, const std::vector<MediaSample>& mediaSamples)
+  {
+    //    fprintf(stderr, "deliverAudio");
+#ifdef LOCK_FREE
+    std::vector<MediaSample>::const_iterator it = m_audioSamples.push(mediaSamples.begin(), mediaSamples.end());
+    assert(it == mediaSamples.end());
+    return boost::system::error_code();
+#else
+#ifdef SINGLE_QUEUE
+    boost::mutex::scoped_lock l(m_mediaLock);
+    for (const MediaSample& mediaSample : mediaSamples)
+    {
+      if (m_mediaSamples.empty())
+      {
+        m_mediaSamples.push_back(std::make_pair(AUDIO_TYPE, mediaSample));
+      }
+      else
+      {
+        for (std::deque<MediaSample_t>::reverse_iterator it = m_mediaSamples.rbegin();
+          it != m_mediaSamples.rend(); ++it)
+        {
+          if (mediaSample.getPresentationTime() > it->second.getPresentationTime())
+          {
+            m_mediaSamples.insert(it.base(), std::make_pair(AUDIO_TYPE, mediaSample));
+            break;
+          }
+        }
+      }
+    }
+    m_dQueueDuration = m_mediaSamples.back().second.getPresentationTime() - m_mediaSamples.front().second.getPresentationTime();
+    if (m_dQueueDuration >= MAX_QUEUE_DURATION_SECONDS)
+    {
+      VLOG(5) << "Queue duration is longer than " << MAX_QUEUE_DURATION_SECONDS << "s: " << m_dQueueDuration << "s";
+    }
+#if 0
+    printQueueInfo(m_mediaSamples);
+#else
+    printQueueInfo2(m_mediaSamples);
+#endif
+#else
+    boost::mutex::scoped_lock l(m_audioLock);
+    m_audioSamples.insert(m_audioSamples.end(), mediaSamples.begin(), mediaSamples.end());
+#endif
+    return boost::system::error_code();
+#endif
+  }
+
+  boost::optional<MediaSample> PacketManagerMediaChannel::getVideo()
+  {
+#ifdef LOCK_FREE
+    MediaSample mediaSample;
+    if (m_videoSamples.pop(mediaSample))
+    {
+      return boost::optional<MediaSample>(mediaSample);
+    }
+    else
+    {
+      // empty
+      return boost::optional<MediaSample>();
+    }
+#else
+#ifdef SINGLE_QUEUE
+    boost::mutex::scoped_lock l(m_mediaLock);
+    // make sure there is at least one audio sample in the queue so we can align the two streams
+    auto itAudio = std::find_if(m_mediaSamples.begin(), m_mediaSamples.end(), [](const MediaSample_t& pair)
+    {
+      return pair.first == AUDIO_TYPE;
+    });
+    if (itAudio == m_mediaSamples.end())
+    {
+#if 0
+      VLOG(2) << "No audio in queue to align to";
+#endif
+      return boost::optional<MediaSample>();
+    }
+    if (!m_mediaSamples.empty())
+    {
+      MediaSample_t& mediaSample = m_mediaSamples.front();
+      if (mediaSample.first == VIDEO_TYPE)
+      {
+        MediaSample_t mediaSampleCopy = mediaSample;
+        m_mediaSamples.pop_front();
+        return boost::optional<MediaSample>(mediaSampleCopy.second);
+      }
+    }
     return boost::optional<MediaSample>();
-  }
 #else
-  boost::mutex::scoped_lock l(m_videoLock);
-  if (!m_videoSamples.empty())
-  {
-    MediaSample mediaSample = m_videoSamples.front();
-    m_videoSamples.pop_front();
-    return boost::optional<MediaSample>(mediaSample);
-  }
-  return boost::optional<MediaSample>();
+    boost::mutex::scoped_lock l(m_videoLock);
+    if (!m_videoSamples.empty())
+    {
+      MediaSample mediaSample = m_videoSamples.front();
+#if 1
+      MediaSample& lastMediaSample = m_videoSamples.back();
+      VLOG(2) << "Getting video " << mediaSample.getPresentationTime() << " Last in queue: " << lastMediaSample.getPresentationTime() << " queue size: " << m_videoSamples.size();
 #endif
-}
-
-boost::optional<MediaSample> PacketManagerMediaChannel::getAudio()
-{
-#ifdef LOCK_FREE
-  MediaSample mediaSample;
-  if (m_audioSamples.pop(mediaSample))
-  {
-    return boost::optional<MediaSample>(mediaSample);
-  }
-  else
-  {
-    // empty
+      m_videoSamples.pop_front();
+      return boost::optional<MediaSample>(mediaSample);
+    }
     return boost::optional<MediaSample>();
-  }
-#else
-  boost::mutex::scoped_lock l(m_audioLock);
-  if (!m_audioSamples.empty())
-  {
-    MediaSample mediaSample = m_audioSamples.front();
-    m_audioSamples.pop_front();
-    return boost::optional<MediaSample>(mediaSample);
-  }
-  return boost::optional<MediaSample>();
 #endif
-}
+#endif
+  }
+
+  boost::optional<MediaSample> PacketManagerMediaChannel::getAudio()
+  {
+#ifdef LOCK_FREE
+    MediaSample mediaSample;
+    if (m_audioSamples.pop(mediaSample))
+    {
+      return boost::optional<MediaSample>(mediaSample);
+    }
+    else
+    {
+      // empty
+      return boost::optional<MediaSample>();
+    }
+#else
+#ifdef SINGLE_QUEUE
+    boost::mutex::scoped_lock l(m_mediaLock);
+    // make sure there is at least one video sample in the queue so we can align the two streams
+    auto itVideo = std::find_if(m_mediaSamples.begin(), m_mediaSamples.end(), [](const MediaSample_t& pair)
+    {
+      return pair.first == VIDEO_TYPE;
+    });
+    if (itVideo == m_mediaSamples.end())
+    {
+#if 0
+      VLOG(2) << "No video in queue to align to";
+#endif
+      return boost::optional<MediaSample>();
+    }
+    if (!m_mediaSamples.empty())
+    {
+      MediaSample_t& mediaSample = m_mediaSamples.front();
+      if (mediaSample.first == AUDIO_TYPE)
+      {
+        MediaSample_t mediaSampleCopy = mediaSample;
+        m_mediaSamples.pop_front();
+        return boost::optional<MediaSample>(mediaSampleCopy.second);
+      }
+    }
+    return boost::optional<MediaSample>();
+#else
+    boost::mutex::scoped_lock l(m_audioLock);
+    if (!m_audioSamples.empty())
+    {
+      MediaSample mediaSample = m_audioSamples.front();
+#if 1
+      MediaSample& lastMediaSample = m_audioSamples.back();
+      VLOG(2) << "Getting audio " << mediaSample.getPresentationTime() << " Last in queue: " << lastMediaSample.getPresentationTime() << " queue size: " << m_audioSamples.size();
+#endif
+      m_audioSamples.pop_front();
+      return boost::optional<MediaSample>(mediaSample);
+    }
+    return boost::optional<MediaSample>();
+#endif
+#endif
+  }
 
 } //lme
-
